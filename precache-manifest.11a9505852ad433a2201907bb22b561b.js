641e4e51b0afbe5aba7162404bc330900a050a64
Usman Ali <usmanali175@yahoo.com>
2021-06-23 03:21:42 +0500

    Updates

---
+++ precache-manifest.11a9505852ad433a2201907bb22b561b.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9319ab4c096fe779b679e85cf89714aa",
+    "url": "/watchTrade_FrontEnd/index.html"
+  },
+  {
+    "revision": "bae2f1aa00da7b963f51",
+    "url": "/watchTrade_FrontEnd/static/css/2.91044456.chunk.css"
+  },
+  {
+    "revision": "7d9318919c5627839fe1",
+    "url": "/watchTrade_FrontEnd/static/css/3.d097daa9.chunk.css"
+  },
+  {
+    "revision": "3c20b1f19a9bfe897b1e",
+    "url": "/watchTrade_FrontEnd/static/css/4.e58bd043.chunk.css"
+  },
+  {
+    "revision": "b728b19a99f96429c62a",
+    "url": "/watchTrade_FrontEnd/static/css/main.0972c2b2.chunk.css"
+  },
+  {
+    "revision": "bae2f1aa00da7b963f51",
+    "url": "/watchTrade_FrontEnd/static/js/2.6d363913.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/watchTrade_FrontEnd/static/js/2.6d363913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9318919c5627839fe1",
+    "url": "/watchTrade_FrontEnd/static/js/3.85fcb9db.chunk.js"
+  },
+  {
+    "revision": "3c20b1f19a9bfe897b1e",
+    "url": "/watchTrade_FrontEnd/static/js/4.1d9f8bda.chunk.js"
+  },
+  {
+    "revision": "ef85bad8127cb18cb440d52c0c1b82d1",
+    "url": "/watchTrade_FrontEnd/static/js/4.1d9f8bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b728b19a99f96429c62a",
+    "url": "/watchTrade_FrontEnd/static/js/main.87d77e94.chunk.js"
+  },
+  {
+    "revision": "d60e2325bf7eae1bc57d",
+    "url": "/watchTrade_FrontEnd/static/js/runtime-main.28c22415.js"
+  },
+  {
+    "revision": "10df8b6780b76c4623090d96cf37d507",
+    "url": "/watchTrade_FrontEnd/static/media/1.10df8b67.png"
+  },
+  {
+    "revision": "c07711d0c31941dd4398a8193469a61b",
+    "url": "/watchTrade_FrontEnd/static/media/2.c07711d0.png"
+  },
+  {
+    "revision": "a9c9391c039910deb0c5c96efc2d6e5b",
+    "url": "/watchTrade_FrontEnd/static/media/3.a9c9391c.png"
+  },
+  {
+    "revision": "0d90aa2dd6c2590b3097356e3916b9b9",
+    "url": "/watchTrade_FrontEnd/static/media/4.0d90aa2d.png"
+  },
+  {
+    "revision": "8683324929b842ab8cf166818d04e631",
+    "url": "/watchTrade_FrontEnd/static/media/5.86833249.png"
+  },
+  {
+    "revision": "86421e325e13258b2ebfabf924dba09d",
+    "url": "/watchTrade_FrontEnd/static/media/6.86421e32.png"
+  },
+  {
+    "revision": "76449b51990725bc654be7a0fe60ff68",
+    "url": "/watchTrade_FrontEnd/static/media/7.76449b51.png"
+  },
+  {
+    "revision": "f83d8fe5be49a440ef31bcdb1fb7f8ab",
+    "url": "/watchTrade_FrontEnd/static/media/about-banner.f83d8fe5.png"
+  },
+  {
+    "revision": "8ac91aac3d9db255eb42f4b825de96ad",
+    "url": "/watchTrade_FrontEnd/static/media/bg-1.8ac91aac.png"
+  },
+  {
+    "revision": "ea5ba3084afd6fc6a1a80cecb5c624fb",
+    "url": "/watchTrade_FrontEnd/static/media/bg-1.ea5ba308.jpg"
+  },
+  {
+    "revision": "f73b7ced3d3386634d0002d1be61e1ff",
+    "url": "/watchTrade_FrontEnd/static/media/bg-2.f73b7ced.png"
+  },
+  {
+    "revision": "42334bb2bd34f155c9a95e1f5ab521f4",
+    "url": "/watchTrade_FrontEnd/static/media/bg-3.42334bb2.png"
+  },
+  {
+    "revision": "bbf1bacba5a4991b87e846cde1b23a66",
+    "url": "/watchTrade_FrontEnd/static/media/bg.bbf1bacb.png"
+  },
+  {
+    "revision": "03937b78d87b78157d67af03c731bb85",
+    "url": "/watchTrade_FrontEnd/static/media/contact-top.03937b78.png"
+  },
+  {
+    "revision": "fbf38af6ba02c4baf30796640cc6a293",
+    "url": "/watchTrade_FrontEnd/static/media/detail-1.fbf38af6.png"
+  },
+  {
+    "revision": "9ae443373185c3d186564cea57a2e445",
+    "url": "/watchTrade_FrontEnd/static/media/detail-2.9ae44337.png"
+  },
+  {
+    "revision": "540fad222d9a954a78be1f5bb33999c1",
+    "url": "/watchTrade_FrontEnd/static/media/detail-3.540fad22.png"
+  },
+  {
+    "revision": "d6d4a831b01618e3feef23c47b220b78",
+    "url": "/watchTrade_FrontEnd/static/media/detail-4.d6d4a831.png"
+  },
+  {
+    "revision": "fffa191b9fc9625f3e5d6969bc12dfbf",
+    "url": "/watchTrade_FrontEnd/static/media/foot-2.fffa191b.jpg"
+  },
+  {
+    "revision": "b81c6df138e320d57cfa723660e5a707",
+    "url": "/watchTrade_FrontEnd/static/media/human-1.b81c6df1.png"
+  },
+  {
+    "revision": "49c7d041e31df38c9ee865a9b050dd23",
+    "url": "/watchTrade_FrontEnd/static/media/profile-1.49c7d041.png"
+  },
+  {
+    "revision": "d985b8675461dbece75100dadc671ba9",
+    "url": "/watchTrade_FrontEnd/static/media/sell-watch.d985b867.png"
+  },
+  {
+    "revision": "a38b1e7aa9593a46a835528cf08984e9",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-1.a38b1e7a.png"
+  },
+  {
+    "revision": "d30b9153b7d3034d8180da7ce3c4264c",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-1.d30b9153.png"
+  },
+  {
+    "revision": "4e57ebec93794b83c188e7e1133c6d8e",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-2.4e57ebec.png"
+  },
+  {
+    "revision": "93e4c36840f807877014eeaeac51c7e0",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-2.93e4c368.png"
+  },
+  {
+    "revision": "61a4660c72bb620e0bb49dcb518a7c8d",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-3.61a4660c.png"
+  },
+  {
+    "revision": "ecf0746219a2c628caceb016d7d74b21",
+    "url": "/watchTrade_FrontEnd/static/media/slider-image-3.ecf07462.png"
+  },
+  {
+    "revision": "94e5d39f1ea915a435150c9f556445a4",
+    "url": "/watchTrade_FrontEnd/static/media/watch-1.94e5d39f.jpg"
+  },
+  {
+    "revision": "e48f93541cbcdec3c81c0d959e62621f",
+    "url": "/watchTrade_FrontEnd/static/media/watch-10.e48f9354.jpg"
+  },
+  {
+    "revision": "3adaa4bb0c1925ab498b6493d79f58ed",
+    "url": "/watchTrade_FrontEnd/static/media/watch-15.3adaa4bb.jpg"
+  },
+  {
+    "revision": "146a27c3284ef5413a05487c624eb989",
+    "url": "/watchTrade_FrontEnd/static/media/watch-1a.146a27c3.jpg"
+  },
+  {
+    "revision": "67bad0123a5f7aa924df1a340a18433e",
+    "url": "/watchTrade_FrontEnd/static/media/watch-2.67bad012.jpg"
+  },
+  {
+    "revision": "db0970736988db53e06b758c0fd74a78",
+    "url": "/watchTrade_FrontEnd/static/media/watch-2a.db097073.jpg"
+  },
+  {
+    "revision": "f3f41e0adeca5b9c09480fcb4a96028e",
+    "url": "/watchTrade_FrontEnd/static/media/watch-3.f3f41e0a.jpg"
+  },
+  {
+    "revision": "9d1989448840a13ec2943f5ce499ca56",
+    "url": "/watchTrade_FrontEnd/static/media/watch-3a.9d198944.jpg"
+  },
+  {
+    "revision": "445fead61aec89d78258692eb535b802",
+    "url": "/watchTrade_FrontEnd/static/media/watch-4.445fead6.jpg"
+  },
+  {
+    "revision": "2fecd8cdcd018ba8f7f40f63c8f6917c",
+    "url": "/watchTrade_FrontEnd/static/media/watch-4a.2fecd8cd.jpg"
+  },
+  {
+    "revision": "297cb9a86b36fb2a650c56b2c7a97cc1",
+    "url": "/watchTrade_FrontEnd/static/media/watch-5.297cb9a8.jpg"
+  },
+  {
+    "revision": "5114ce6e2593dddde22746c0ed805a5d",
+    "url": "/watchTrade_FrontEnd/static/media/watch-7.5114ce6e.jpg"
+  },
+  {
+    "revision": "f091d69f3d58052c09c7fff5a423db9f",
+    "url": "/watchTrade_FrontEnd/static/media/watch-8.f091d69f.jpg"
+  },
+  {
+    "revision": "185a2c4894d51bb0cc0ac07e4f31e0a0",
+    "url": "/watchTrade_FrontEnd/static/media/watch.185a2c48.png"
+  }
+]);
