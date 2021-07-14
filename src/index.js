e8a0d1d1fd139d886dd9d61ebd4dc7bbd56f2d3c
Syed Zohaib Hussain <roxas.zohbi786@gmail.com>
2021-07-14 21:39:55 +0500

    adminPanel 1st Commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
-import 'react-app-polyfill/ie11'; // For IE 11 support
-import 'react-app-polyfill/stable';
-import './polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*!
 
-import { icons } from './assets/icons'
+=========================================================
+* Light Bootstrap Dashboard React - v2.0.0
+=========================================================
 
-import { Provider } from 'react-redux'
-import configureStore from "./Redux/Store";
-// import store from './store'
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
 
-React.icons = icons
-const store = configureStore();
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
+import "./assets/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import AdminLayout from "layouts/Admin.js";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store.store}>
-      <App/>
-    </Provider>
-  </React.StrictMode>, 
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
